770bd5123201f79d19636bbe61325c9ba077fc63
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-10 19:18:44 +0530

    Update form.py

--- cloneit/cloneapp/form.py
+++ cloneit/cloneapp/form.py
@@ -2,7 +2,11 @@ from django.forms import ModelForm
 from django import forms
 
 class RegisterForm(forms.Form):
-	username = forms.CharField(max_length=100)
-	email = forms.EmailField(max_length=100)
-	password = forms.CharField(max_length=30,widget=forms.PasswordInput)
-	confirm_password = forms.CharField(max_length=30,widget=forms.PasswordInput)
+	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user'}))
+	email = forms.EmailField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control mail'}))
+	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass'}))
+	confirm_password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control confpass'}))
+
+class SigninForm(forms.Form):
+	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user'}))
+	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass'}))
